54e0fb6927b092ccb8f5f4d0d4e7fcaf3dde9b1a
Raza Fatma <razafatma607@email.com>
2025-01-11 16:52:08 +0530

    initial commit

--- scrite.js
+++ scrite.js
@@ -1,7 +1,15 @@
-const menuToggle = document.querySelector('.menu-toggle');
-const sidebar = document.querySelector('.sidebar');
+const menuToggle = document.querySelector(".menu-toggle");
+const sidebar = document.querySelector(".sidebar");
+const menuBtnIcon = menuToggle.querySelector("i");
 
-menuToggle.addEventListener('click', () => {
-    sidebar.classList.toggle('active');
+menuToggle.addEventListener("click", (e) => {
+  sidebar.classList.toggle("active");
+
+  const isOpen = sidebar.classList.contains("active");
+  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
 });
 
+navLinks.addEventListener("click", (e) => {
+  sidebar.classList.remove("active");
+  menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
